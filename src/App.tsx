29a6c5665fc650cc5e04df4aa43f8f554c61947b
liyo1242 <liyo1242@gmail.com>
2022-06-11 10:15:42 +0800

    chore: adjust folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,50 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { Group } from '@vx/group';
-import { hierarchy, Tree } from '@vx/hierarchy';
-import { LinearGradient } from '@vx/gradient';
-import { pointRadial } from 'd3-shape';
-import useForceUpdate from './useForceUpdate';
-import getLinkComponent from './getLinkComponent';
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { Group } from '@vx/group'
+import { hierarchy, Tree } from '@vx/hierarchy'
+import { LinearGradient } from '@vx/gradient'
+import { pointRadial } from 'd3-shape'
+import getLinkComponent from './component/getLinkComponent'
 import classes from './App.module.css'
 
-import { useResize } from './useResize'
+import { useResize } from './hook/useResize'
+import useForceUpdate from './hook/useForceUpdate'
 
 interface TreeNode {
-  name: string;
-  isExpanded?: boolean;
-  isEdited?: boolean;
-  _id?: string;
-  children?: TreeNode[];
+  name: string
+  isExpanded?: boolean
+  isEdited?: boolean
+  _id?: string
+  children?: TreeNode[]
 }
 
 const originData = {
-  'type': {
-    'product': {
-      'enterprise': {
-        'category': {
-          'setting': {
-            'period': {
-              'type': 'integer',
-              'enum': [30, 365]
-            }
+  type: {
+    product: {
+      enterprise: {
+        category: {
+          setting: {
+            period: {
+              type: 'integer',
+              enum: [30, 365],
+            },
           },
-          'service_setting': {
-            'template': {
-              'type': 'integer',
-              'default': 10,
-              'maximun': 50,
-              'minimum': 5
-            }
-          }
-        }
-      }
-    }
-  }
+          service_setting: {
+            template: {
+              type: 'integer',
+              default: 10,
+              maximun: 50,
+              minimum: 5,
+            },
+          },
+        },
+      },
+    },
+  },
 }
 
 type JsonTransFormTreeNode = {
-  (data: object): Array<TreeNode>;
+  (data: object): Array<TreeNode>
 }
 
 const jsonTransFormTreeNode: JsonTransFormTreeNode = (data) => {
@@ -53,7 +53,7 @@ const jsonTransFormTreeNode: JsonTransFormTreeNode = (data) => {
       return {
         name: key,
         _id: uuidv4(),
-        children: jsonTransFormTreeNode(val)
+        children: jsonTransFormTreeNode(val),
       }
     }
     if (Array.isArray(val)) {
@@ -62,70 +62,70 @@ const jsonTransFormTreeNode: JsonTransFormTreeNode = (data) => {
         _id: uuidv4(),
         children: val.map((v) => ({
           name: v + '',
-          _id: uuidv4()
-        }))
+          _id: uuidv4(),
+        })),
       }
     }
     return {
       name: key,
       _id: uuidv4(),
-      children: [{
-        name: val + '',
-        _id: uuidv4()
-      }]
-    } 
+      children: [
+        {
+          name: val + '',
+          _id: uuidv4(),
+        },
+      ],
+    }
   })
 }
 
 const data: TreeNode = {
   name: 'Main',
-  children: jsonTransFormTreeNode(originData)
-};
+  children: jsonTransFormTreeNode(originData),
+}
 
-const defaultMargin = { top: 30, left: 30, right: 30, bottom: 70 };
+const defaultMargin = { top: 30, left: 30, right: 30, bottom: 70 }
 
 export type LinkTypesProps = {
-  margin?: { top: number; right: number; bottom: number; left: number };
-};
+  margin?: { top: number; right: number; bottom: number; left: number }
+}
 
-export default function ({
-  margin = defaultMargin
-}: LinkTypesProps) {
+export default function ({ margin = defaultMargin }: LinkTypesProps) {
   const [currentEditing, setCurrentEditing] = useState<string>('')
-  const [layout, setLayout] = useState<string>('cartesian');
-  const [orientation, setOrientation] = useState<string>('horizontal');
-  const [linkType, setLinkType] = useState<string>('diagonal');
-  const [stepPercent, setStepPercent] = useState<number>(0.1);
-  const forceUpdate = useForceUpdate();
+  const [layout] = useState<string>('cartesian')
+  const [orientation] = useState<string>('horizontal')
+  const [linkType] = useState<string>('diagonal')
+  const [stepPercent] = useState<number>(0.1)
+  const forceUpdate = useForceUpdate()
 
   const { windowHeight: totalHeight, windowWidth: totalWidth } = useResize()
 
-  const innerWidth = totalWidth - margin.left - margin.right;
-  const innerHeight = totalHeight - margin.top - margin.bottom;
+  const innerWidth = totalWidth - margin.left - margin.right
+  const innerHeight = totalHeight - margin.top - margin.bottom
 
-  let origin: { x: number; y: number };
-  let sizeWidth: number;
-  let sizeHeight: number;
+  let origin: { x: number; y: number }
+  let sizeWidth: number
+  let sizeHeight: number
 
   if (layout === 'polar') {
     origin = {
       x: innerWidth / 2,
       y: innerHeight / 2,
-    };
-    sizeWidth = 2 * Math.PI;
-    sizeHeight = Math.min(innerWidth, innerHeight) / 2;
+    }
+    sizeWidth = 2 * Math.PI
+    sizeHeight = Math.min(innerWidth, innerHeight) / 2
   } else {
-    origin = { x: 0, y: 0 };
+    origin = { x: 0, y: 0 }
     if (orientation === 'vertical') {
-      sizeWidth = innerWidth;
-      sizeHeight = innerHeight;
+      sizeWidth = innerWidth
+      sizeHeight = innerHeight
     } else {
-      sizeWidth = innerHeight;
-      sizeHeight = innerWidth;
+      sizeWidth = innerHeight
+      sizeHeight = innerWidth
     }
   }
 
-  const LinkComponent = getLinkComponent({ layout, linkType, orientation });
+  const LinkComponent = getLinkComponent({ layout, linkType, orientation })
 
   return totalWidth < 10 ? null : (
     <div>
@@ -133,11 +133,11 @@ export default function ({
         <LinearGradient id="links-gradient" from="#fd9b93" to="#fe6e9e" />
         <Group top={margin.top} left={margin.left}>
           <Tree
-            root={hierarchy(data, d => (d.isExpanded ? null : d.children))}
+            root={hierarchy(data, (d) => (d.isExpanded ? null : d.children))}
             size={[sizeWidth, sizeHeight]}
             separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
           >
-            {tree => (
+            {(tree) => (
               <Group top={origin.y} left={origin.x}>
                 {tree.links().map((link, i) => (
                   <LinkComponent
@@ -151,21 +151,21 @@ export default function ({
                 ))}
 
                 {tree.descendants().map((node, key) => {
-                  const width = 80;
-                  const height = 40;
+                  const width = 80
+                  const height = 40
 
-                  let top: number;
-                  let left: number;
+                  let top: number
+                  let left: number
                   if (layout === 'polar') {
-                    const [radialX, radialY] = pointRadial(node.x, node.y);
-                    top = radialY;
-                    left = radialX;
+                    const [radialX, radialY] = pointRadial(node.x, node.y)
+                    top = radialY
+                    left = radialX
                   } else if (orientation === 'vertical') {
-                    top = node.y;
-                    left = node.x;
+                    top = node.y
+                    left = node.x
                   } else {
-                    top = node.x;
-                    left = node.y;
+                    top = node.x
+                    left = node.y
                   }
 
                   return (
@@ -175,9 +175,9 @@ export default function ({
                           r={12}
                           fill="url('#links-gradient')"
                           onClick={() => {
-                            node.data.isExpanded = !node.data.isExpanded;
-                            console.log(node);
-                            forceUpdate();
+                            node.data.isExpanded = !node.data.isExpanded
+                            console.log(node)
+                            forceUpdate()
                           }}
                         />
                       )}
@@ -194,18 +194,25 @@ export default function ({
                           strokeOpacity={node.data.children ? 1 : 0.6}
                           rx={node.data.children ? 0 : 10}
                           onClick={() => {
-                            node.data.children && (node.data.isExpanded = !node.data.isExpanded);
-                            !node.data.children && (setCurrentEditing(node.data._id || ''))
-                            forceUpdate();
+                            node.data.children && (node.data.isExpanded = !node.data.isExpanded)
+                            !node.data.children && setCurrentEditing(node.data._id || '')
+                            forceUpdate()
                           }}
                         />
                       )}
-                      {(!node.data.children && currentEditing === node.data._id) &&
+                      {!node.data.children && currentEditing === node.data._id && (
                         <foreignObject x={-width / 2} y={-height / 2} width={width} height={height}>
-                          <input className={classes.treeNodeInput} value={node.data.name} onChange={e => {node.data.name = e.target.value;forceUpdate();}}></input>
+                          <input
+                            className={classes.treeNodeInput}
+                            value={node.data.name}
+                            onChange={(e) => {
+                              node.data.name = e.target.value
+                              forceUpdate()
+                            }}
+                          ></input>
                         </foreignObject>
-                      }
-                      {(node.data.children || currentEditing !== node.data._id) &&
+                      )}
+                      {(node.data.children || currentEditing !== node.data._id) && (
                         <text
                           dy=".33em"
                           fontSize={9}
@@ -216,9 +223,9 @@ export default function ({
                         >
                           {node.data.name}
                         </text>
-                      }
+                      )}
                     </Group>
-                  );
+                  )
                 })}
               </Group>
             )}
@@ -226,5 +233,5 @@ export default function ({
         </Group>
       </svg>
     </div>
-  );
+  )
 }
---
+++ src/hook/useResize.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+export const useResize = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight)
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+    setWindowHeight(window.innerHeight)
+  }
+
+  useEffect(() => {
+    window.onresize = handleResize
+
+    return () => {
+      window.onresize = null
+    }
+  }, [])
+
+  return {
+    windowWidth,
+    windowHeight,
+  }
+}
